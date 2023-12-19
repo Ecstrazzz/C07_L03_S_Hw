e0187c2e3ed5fb81aabf7d122771f47a9b5b794b
Ecs <ecstrazzz@ya.ru>
2023-12-19 22:43:51 +0500

    added a third homework assignment

--- Program.cs
+++ Program.cs
@@ -31,16 +31,55 @@
 //
 // Решение:
 // 
-int[] array = new int[10];
-int count = 0;
-for (int i = 0; i < array.Length; i++)
+// int[] array = new int[10];
+// int count = 0;
+// for (int i = 0; i < array.Length; i++)
+// {
+//     array[i] = new Random().Next(-10, 11);
+//     Console.Write(array[i] + " ");
+//     if (array[i] % 2 == 0)
+//     {
+//         count++;
+//     }
+// }
+// Console.WriteLine();
+// Console.WriteLine("Количество чётных чисел = " + count);
+//
+//-----------------------------------------------------
+//
+// Задача 3: Задайте массив из вещественных чисел с
+// ненулевой дробной частью. Найдите разницу между
+// максимальным и минимальным элементов массива.
+//
+// Решение:
+//
+Console.WriteLine("Введите размер массива");
+int count = Convert.ToInt32(Console.ReadLine());
+double[] array = new double[count];
+
+for (int i = 0; i < count; i++)
 {
-    array[i] = new Random().Next(-10, 11);
+    array[i] = new Random().Next(101, 10000);
+    array[i] /= 100;
     Console.Write(array[i] + " ");
-    if (array[i] % 2 == 0)
+}
+
+Console.WriteLine();
+
+double min = array[0];
+double max = array[0];
+
+for (int i = 0; i < count; i++)
+{
+    if (array[i] > max)
     {
-        count++;
+        max = array[i];
+    }
+    else if (array[i] < min)
+    {
+        min = array[i];
     }
 }
-Console.WriteLine();
-Console.WriteLine("Количество чётных чисел = " + count);
+
+Console.WriteLine($"\nМинимальный = {min} | Максимальный = {max}");
+Console.WriteLine($"\nРазница между Мин и Макс злементом = {max - min}");
